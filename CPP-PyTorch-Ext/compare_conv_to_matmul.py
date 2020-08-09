240695a2ff3365f820b9da4e61d0bbc0f6a8262f
Amir Ofir <amir.israel.ofir@gmail.com>
2020-08-09 14:59:48 +0300

    compare Yolo networks

--- CPP-PyTorch-Ext/compare_conv_to_matmul.py
+++ CPP-PyTorch-Ext/compare_conv_to_matmul.py
@@ -42,14 +42,36 @@ def compareTimes(batch_size: int, in_channels: int, out_channels: int, input_dim
     return durationConv, durationMatmul
 
 
-batch_sizes_arr = [1]#, 5,10]
-in_channels_arr = [1,8]#4,8,16, 32, 64, 128, 256, 512]
-out_channels_arr = [8,16,32,64,128]
+batch_sizes_arr = [1]
+in_channels_arr = [1,4,8,16,32]
+out_channels_arr = [4, 8,16,32,64,128]
 input_dims = [16, 32, 64, 128, 256, 512, 650, 1024, 1280, 1500, 1920]
 print('{batch_size}, {in_channels}, {out_channels}, {input_dim}, {durationConv}, {durationMatmul}, {diff}')
 
 cross = [(a,b,c,d) for a in batch_sizes_arr for b in in_channels_arr for c in out_channels_arr for d in input_dims]
+
+# YOLO-LITE
+cross.append((1,3,16,224))
+cross.append((1,16,32,112))
+cross.append((1,32,64,56))
+cross.append((1,64,128,28))
+cross.append((1,128,128,14))
+cross.append((1,128,256,7))
+
+# YoloV3
+cross.append((1,3,32,416))
+cross.append((1,32,64,208))
+cross.append((1,64,32,104))
+cross.append((1,32,64,52))
+cross.append((1,64,128,26))
+cross.append((1,128,128,13))
+cross.append((1,128,256,13))
+cross.append((1,256,512,13))
+cross.append((1,256,512,7))
+cross.append((1,512,512,7))
+cross.append((1,512,1024,7))
 for (batch_size, in_channels, out_channels, input_dim) in cross:
     durationConv, durationMatmul = compareTimes(batch_size, in_channels, out_channels, input_dim)
-    diff = durationConv - durationMatmul
-    print(f'{batch_size}, {in_channels}, {out_channels}, {input_dim}, {durationConv}, {durationMatmul},{diff}')
+    diff = round(durationConv - durationMatmul, 5)
+    per = round((durationMatmul / durationConv) * 100, 5)
+    print(f'{batch_size}, {in_channels}, {out_channels}, {input_dim}, {durationConv}, {durationMatmul}, {diff}, {per}%')
