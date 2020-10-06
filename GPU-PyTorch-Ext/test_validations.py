21e3cc4f636ae66c6bcebcbdfdacb74c5defab64
Amir <amir@quickode.com>
2020-10-06 18:20:21 +0300

    test matmul_only

--- GPU-PyTorch-Ext/test_validations.py
+++ GPU-PyTorch-Ext/test_validations.py
@@ -9,22 +9,36 @@ from GCK_GPU_CPP import matmul_only
 
 kernel_dim = 3
 
-def tensors_equal(a,b):
+def print_tensors_equal(a,b):
     b = torch.allclose(a, b, atol=0.01)
     if (b):
         print('same: True')
     else:
         print('Same: False (diff:', ((a-b).max()), ')')
 
-def compareResults(batch_size: int, in_channels: int, out_channels: int, input_dim: int):
-    input = torch.randint(0,100, (batch_size, in_channels, input_dim, input_dim), dtype=torch.float32)
-    kernels = torch.ones(out_channels, in_channels, kernel_dim, kernel_dim, dtype=torch.float32)
-    expected = F.conv2d(input, kernels)
+def test_matmul_only(batch_size: int, in_channels: int, out_channels: int, input_dim: int):
+#  LinCombs shape: (out_channels, in_channels*9). Assumed to be contiguous
+#  basisResultsTensor shape: (in_channels*9, H*W). Assumed to be contiguous. values are discarded
+#  colwiseResults shape (3, inputDim * resultDim). input_dim rows, result_dim cols
+    input = torch.randint(0,100, (batch_size, in_channels, input_dim, input_dim), dtype=torch.float32).cuda()
+    linCombs = torch.randint(0,100, (out_channels, in_channels*9), requires_grad=False, dtype=torch.float32).cuda().contiguous()
+    basisResultsTensor = torch.randint(0, 100, (in_channels*9, (input_dim-2)**2), requires_grad=False, dtype=torch.float32).cuda().contiguous()
+    colwiseResults = torch.empty(5000, requires_grad=False, dtype=torch.float32).cuda().contiguous()
+    result = matmul_only(input, linCombs, basisResultsTensor, colwiseResults)
+    print(linCombs.shape, basisResultsTensor.shape )
+    expected = linCombs @ basisResultsTensor
+    for i in range(batch_size):
+        print_tensors_equal(result[i], expected)
+    
+# def compareResults(batch_size: int, in_channels: int, out_channels: int, input_dim: int):
+#     input = torch.randint(0,100, (batch_size, in_channels, input_dim, input_dim), dtype=torch.float32)
+#     kernels = torch.ones(out_channels, in_channels, kernel_dim, kernel_dim, dtype=torch.float32)
+#     expected = F.conv2d(input, kernels)
 
-    layer = GCK3x3Layer(in_channels, out_channels, 3, False, input_dim - 2, kernels)
-    result = layer.forward(input)
+#     layer = GCK3x3Layer(in_channels, out_channels, 3, False, input_dim - 2, kernels)
+#     result = layer.forward(input)
 
-    tensors_equal(expected, result)
+#     tensors_equal(expected, result)
 
 
 lst = [
@@ -36,4 +50,5 @@ lst = [
     ]
 
 for batch_size, in_channels, out_channels, input_dim in lst:
-    compareResults(batch_size, in_channels, out_channels, input_dim)
+    test_matmul_only(batch_size, in_channels, out_channels, input_dim)
+    # compareResults(batch_size, in_channels, out_channels, input_dim)
